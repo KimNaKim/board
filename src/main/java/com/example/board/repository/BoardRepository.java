10f755a6ae46a3d931e41496e809411b194a19e5
KimNaKim <shila66@naver.com>
2026-02-13 09:47:05 +0900

    테이블 객체

---
+++ src/main/java/com/example/board/repository/BoardRepository.java
@@ -0,0 +1,11 @@
+package com.example.board.repository;
+
+import org.springframework.stereotype.Repository;
+
+import lombok.RequiredArgsConstructor;
+
+@RequiredArgsConstructor
+@Repository
+public class BoardRepository {
+
+}
